970c2fef5befc64f59fe611a52fb1cf52fde7c13
wangyiyi2 <61692465+wangyiyi2@users.noreply.github.com>
2020-03-08 18:55:11 +0800

    Update parse_add.py

--- wlb/parse_add.py
+++ wlb/parse_add.py
@@ -6,6 +6,7 @@ AREA_SUFFIXES = ["自治区", "省", "市", "区", "县"]
 PRO_CODE_LEN = 10
 CIT_CODE_LEN = 15
 DIS_CODE_LEN = 20
+IS_AC = True
 data = pd.DataFrame(pd.read_csv('wlb/area.txt', encoding='utf-8'))
 area_name = data['name'].values
 model = ac.Trie(area_name)
@@ -32,9 +33,14 @@ def parse_address(text):
         "street": "",
         "other": ""
     }
-    get_address(text, "", address)
-    fill_parent(address)
-    get_address(text, AREA_SUFFIXES, address)
+    if IS_AC:
+        get_address_ac(text, "", address)
+        fill_parent(address)
+        get_address_ac(text, AREA_SUFFIXES, address)
+    else:
+        get_address(text, "", address)
+        fill_parent(address)
+        get_address(text, AREA_SUFFIXES, address)
     index = find_index(address["district"],text)
     if index != -1:
         text = text[index:]
@@ -46,8 +52,7 @@ def parse_address(text):
     return address
 
 
-def get_address(text, suffix, address):
-    words = []
+def get_address_ac(text, suffix, address):
     if (suffix == ""):
         words = model.search(text)
         for word in words.keys():
@@ -62,27 +67,31 @@ def get_address(text, suffix, address):
                 index = no_suf.index(word)
                 set_address(index,address)
 
-    # word_list = []
-    # i = 0
-    # while i < len(text):
-    #     longest_word = text[i]
-    #     for j in range(i + 1, len(text) + 1):
-    #         word = text[i: j]
-    #         if(suffix == ""):
-    #             if word in data['name'].values:
-    #                 set_address(word,address)
-    #                 longest_word = word
-    #                 break
-    #         else:
-    #             for suf in AREA_SUFFIXES:
-    #                 if word+suf in data['name'].values:
-    #                     set_address(word+suf, address)
-    #                     longest_word = word
-    #                     break
-    #
-    #     word_list.append(longest_word)
-    #     i += len(longest_word)
-    # return word_list
+
+def get_address(text, suffix, address):
+    word_list = []
+    i = 0
+    while i < len(text):
+        longest_word = text[i]
+        for j in range(i + 1, len(text) + 1):
+            word = text[i: j]
+            if(suffix == ""):
+                if word in data['name'].values:
+                    index = list(data['name']).index(word)
+                    set_address(index,address)
+                    longest_word = word
+                    break
+            else:
+                for suf in AREA_SUFFIXES:
+                    if word+suf in data['name'].values:
+                        index = list(data['name']).index(word+suf)
+                        set_address(index, address)
+                        longest_word = word
+                        break
+
+        word_list.append(longest_word)
+        i += len(longest_word)
+    return word_list
 
 
 def set_address(index,address):
@@ -118,3 +127,6 @@ def fill_parent(address):
         index = list(data['name']).index(address['district'])
         address['city'] = data['name'][data['parent_id'][index]-1]
         fill_parent(address)
+
+
+parse_address("湖北武汉")
