5f084bcf53164081bf73d08755786198351e3564
wangyiyi2 <61692465+wangyiyi2@users.noreply.github.com>
2020-03-08 13:08:57 +0800

    Add files via upload

---
+++ wlb/ACautomaton.py
@@ -0,0 +1,94 @@
+# -*- coding:utf-8 -*-
+"""
+Description: AC自动机
+"""
+from collections import defaultdict
+import pandas as pd
+
+
+class TrieNode(object):
+    def __init__(self, value=None):
+        # 值
+        self.value = value
+        # fail指针
+        self.fail = None
+        # 尾标志：标志为i表示第i个模式串串尾，默认为0
+        self.tail = 0
+        # 子节点
+        self.children = {}
+
+
+class Trie(object):
+    def __init__(self, words):
+        # 根节点
+        self.root = TrieNode()
+        # 模式串个数
+        self.count = 0
+        self.words = words
+        for word in words:
+            self.insert(word)
+        self.ac_automation()
+
+    def insert(self, sequence):
+        self.count += 1
+        cur_node = self.root
+        for item in sequence:
+            if item not in cur_node.children:
+                # 插入结点
+                child = TrieNode(value=item)
+                cur_node.children[item] = child
+                cur_node = child
+            else:
+                cur_node = cur_node.children[item]
+        cur_node.tail = self.count
+
+    def ac_automation(self):
+        queue = [self.root]
+        while len(queue):
+            temp_node = queue[0]
+            # 取出队首元素
+            queue.remove(temp_node)
+            for value in temp_node.children.values():
+                # 根的子结点fail指向根自己
+                if temp_node == self.root:
+                    value.fail = self.root
+                else:
+                    # 转到fail指针
+                    p = temp_node.fail
+                    while p:
+                        # 若结点值在该结点的子结点中，则将fail指向该结点的对应子结点
+                        if value.value in p.children:
+                            value.fail = p.children[value.value]
+                            break
+                        # 转到fail指针继续回溯
+                        p = p.fail
+                    # 若为None，表示当前结点值在之前都没出现过，则其fail指向根结点
+                    if not p:
+                        value.fail = self.root
+                # 将当前结点的所有子结点加到队列中
+                queue.append(value)
+
+    def search(self, text):
+        p = self.root
+        # 记录匹配起始位置下标
+        start_index = 0
+        # 成功匹配结果集
+        rst = defaultdict(list)
+        for i in range(len(text)):
+            single_char = text[i]
+            while single_char not in p.children and p is not self.root:
+                p = p.fail
+            if single_char in p.children and p is self.root:
+                start_index = i
+            if single_char in p.children:
+                p = p.children[single_char]
+            else:
+                start_index = i
+                p = self.root
+            temp = p
+            while temp is not self.root:
+                if temp.tail:
+                    rst[self.words[temp.tail - 1]].append((start_index, i))
+                temp = temp.fail
+        return rst
+
